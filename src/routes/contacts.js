a3363474ed9c3702693a5d4669c17e40cb42351d
Raphael Etim <raphaelumoh@gmail.com>
2019-01-10 11:38:17 +0100

    Ch add post route for contact (#7)
    * ch(sms-api):update circle ci config script
    
    * remove config script for circle ci
    
    * ch(sms-api): add more routes for contact

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
-const retrieveAllContacts = require('../controllers/contacts');
+const { retrieveAllContacts, createNewContact, updateExistingContact, removeContact } = require('../controllers/contacts');
+const validateFields = require('../validator/validate');
 
 const contactsRouter = Router();
 
 contactsRouter.route('/')
-  .get(retrieveAllContacts);
+  .get(retrieveAllContacts)
+  .post(validateFields, createNewContact);
+
+contactsRouter.route('/:contactId')
+  .patch(validateFields, updateExistingContact)
+  .delete(removeContact);
+
 
 
 module.exports = contactsRouter;
