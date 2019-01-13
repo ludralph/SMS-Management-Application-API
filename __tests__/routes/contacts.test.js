47d5f73684d46956ea467f99d481b7c5369037fa
ludralph <raphaelumoh@gmail.com>
2019-01-13 14:52:03 +0100

    update contact test

--- __tests__/routes/contacts.test.js
+++ __tests__/routes/contacts.test.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-undef */
 // we will use supertest to test HTTP requests/responses
-const request = require("supertest");
+const request = require('supertest');
 // we also need our app for the correct routes!
 const app = require('../../src/app');
 
-describe('GET /contacts', () => {
-  test('It responds with an array of contacts', async () => {
+describe('GET /contacts', async () => {
+  test('It responds with an array of contacts', async (done) => {
     const response = await request(app).get('/api/v1/contacts');
-    expect(response.body.data.length).toBe(1);
+    expect(response.body.data.length).toBe(0);
     expect(response.statusCode).toBe(200);
+    done();
   });
 });
