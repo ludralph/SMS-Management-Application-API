ed2a8340f30ed992f61286f6b9e720ceabe33fe1
ludralph <raphaelumoh@gmail.com>
2019-01-14 11:54:00 +0100

    add log

--- __tests__/routes/contacts.test.js
+++ __tests__/routes/contacts.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 // we also need our app for the correct routes!
 const app = require('../../src/app');
 console.log('>>>', process.env.NODE_ENV)
+console.log('>>>>', response.body.data)
 describe('GET /contacts', async () => {
   test('It responds with an array of contacts', async (done) => {
     const response = await request(app).get('/api/v1/contacts');
