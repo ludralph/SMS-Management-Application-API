b4996bb994108c5838e700406513dd808263b194
ludralph <raphaelumoh@gmail.com>
2019-01-14 11:50:34 +0100

    update test file

--- __tests__/routes/contacts.test.js
+++ __tests__/routes/contacts.test.js
@@ -3,11 +3,11 @@
 const request = require('supertest');
 // we also need our app for the correct routes!
 const app = require('../../src/app');
-
+console.log('>>>', process.env.NODE_ENV)
 describe('GET /contacts', async () => {
   test('It responds with an array of contacts', async (done) => {
     const response = await request(app).get('/api/v1/contacts');
-    expect(response.body.data.length).toBe(0);
+    expect(response.body.data.length).toBe(1);
     expect(response.statusCode).toBe(200);
     done();
   });
