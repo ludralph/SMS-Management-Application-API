2a4d30b14f90db3240fc644126e0d21c2b0e0bd3
ludralph <raphaelumoh@gmail.com>
2019-01-14 20:30:37 +0100

    add validate smsFields

--- src/validator/validate.js
+++ src/validator/validate.js
@@ -1,14 +1,23 @@
-// import the validator class
 const { validate } = require('jsonschema');
 
-// require the book schema (a JSON file that we generated on jsonschema.net)
 const contactSchema = require('../schemas/contactSchema.json');
+const smsSchema = require('../schemas/smsSchema.json');
 
-const validateFields = (req, res, next) => {
-  // check if the current request.body payload is a valid contact
+const validateContactFields = (req, res, next) => {
   const payload = {};
   payload.data = req.body;
   const result = validate(payload, contactSchema);
+  if (!result.valid) {
+    return next(result.errors.map(error => error.stack));
+  }
+  next();
+};
+
+const validateSmsFields = (req, res, next) => {
+  // check if the current request.body payload is a valid contact
+  const payload = {};
+  payload.data = req.body;
+  const result = validate(payload, smsSchema);
   if (!result.valid) {
     // pass the validation errors to the error handler
     //  the "stack" key is generally the most useful
@@ -17,4 +26,7 @@ const validateFields = (req, res, next) => {
   next();
 };
 
-module.exports = validateFields;
+module.exports = {
+  validateContactFields,
+  validateSmsFields,
+};
